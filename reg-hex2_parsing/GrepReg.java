e3956813e483129e14f302724ba0a940961c5d44
Leomar Duran <29983534+lduran2@users.noreply.github.com>
2021-11-21 18:41:05 -0500

    [reg-hex2_parsing] abstracted out `storeRegExport`

--- reg-hex2_parsing/GrepReg.java
+++ reg-hex2_parsing/GrepReg.java
@@ -2,13 +2,16 @@
  * Grep like tool that prints key headers.
  *
  * By        : Leomar Duran <https://github.com/lduran2/>
- * When      : 2021-11-21t18:07
+ * When      : 2021-11-21t18:41
  * Where     : Temple University
  * For       : CIS 3605
- * Version   : 1.0.0
+ * Version   : 1.0.1
  * Canonical : https://github.com/lduran2/cis3605-intro_digital_forensics/blob/master/reg-hex2_parsing/RegGrep.java
  *
  * CHANGELOG :
+ *     v1.0.1 - 2021-11-21t18:41
+ *         abstracted out `storeRegExport`
+ *
  *     v1.0.0 - 2021-11-21t18:07
  *         store the reg export
  */
@@ -19,7 +22,8 @@ import java.io.PrintStream;
 import java.io.IOException;
 import java.io.IOError;
 
-import java.util.Deque;
+import java.util.Collection;
+//import java.util.Deque;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Map;
@@ -33,37 +37,19 @@ public enum GrepReg {
 	;
 
 	public static void main(final String... args) {
-		String line;
-
 		/* list of the headings */
 		final List<String> HEADINGS = new LinkedList<>();
 		/* the reg export structure */
 		/* maps strings to ordered line sets */
 		final Map<String,List<String>> REG_EXPORT = new TreeMap<>();
-
-		List<String> current_lines = null;
+		/* stores the current lines being added */
+		final List<String> CURRENT_LINES = new LinkedList<>();
 
 		/* buffered reader for STDIN */
 		try (final BufferedReader BRIN = new BufferedReader(
 					new InputStreamReader(in)))
 		{
-			while ((line = BRIN.readLine()) != null) {
-				final int LEN = line.length();
-				/* if an empty line */
-				if (LEN == 0) {
-				}
-				/* if a heading */
-				else if (line.charAt(0) == '[') {
-					/* store heading */
-					HEADINGS.add(line);
-					current_lines = new LinkedList<>();
-					REG_EXPORT.put(line, current_lines);
-				}
-				/* if in a line set */
-				else if (!REG_EXPORT.isEmpty()) {
-					current_lines.add(line);
-				}
-			}
+			storeRegExport(BRIN, HEADINGS, REG_EXPORT, CURRENT_LINES);
 			out.printf("%s", REG_EXPORT);
 		} /* try (new BufferedReader(...System.in)) */
 		catch (final IOException IOE) {
@@ -71,6 +57,48 @@ public enum GrepReg {
 			throw new IOError(IOE);
 		} /* catch (final IOException IOE) */
 	} /* public static void main(final String... args) */
+
+	public static <TStrings extends Collection<String>>
+		void storeRegExport(
+			final BufferedReader src,
+			final TStrings destHeadings,
+			final Map<String,TStrings> destRegExport,
+			final TStrings tmpCurrentLines)
+		throws IOException
+	{
+		/* current line read */
+		String line;
+
+		while ((line = src.readLine()) != null) {
+			/* if an empty line */
+			if (line.length() == 0) {
+				/* pass */
+			} /* if (line.length() == 0) */
+			/* if a heading */
+			else if (line.charAt(0) == '[') {
+				/* store heading */
+				destHeadings.add(line);
+				/* empty the lines */
+				tmpCurrentLines.clear();
+				/* store the line set */
+				destRegExport.put(line, tmpCurrentLines);
+			} /* (line.length() == 0)
+				else if (line.charAt(0) == '[')
+			   */
+			/* if in a line set */
+			else if (!destRegExport.isEmpty()) {
+				/* add this line to the set */
+				tmpCurrentLines.add(line);
+			} /* (line.length() == 0) || (line.charAt(0) == '[')
+				else if (!destRegExport.isEmpty()) {
+			   */
+		} /* while ((line = src.readLine()) != null) */
+	} /* public static <TStrings extends Collection<String>>
+		void storeRegExport(
+			final BufferedReader src,
+			final TStrings destHeadings,
+			Map<String,TStrings> destRegExport)
+	   */
 
 } /* public enum GrepReg */
 
